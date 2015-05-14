c57dad51ec8f2a6ede388eb34c83193895b4398b
Nemanja Mandic <nemanjans84@gmail.com>
2015-05-14 22:30:05 +0200

    Data binding

--- js/controllers.js
+++ js/controllers.js
@@ -4,5 +4,6 @@ var myNameSpace = angular.module('myApp', []);
 myNameSpace.controller('MyController', ['$scope', '$http', function ($scope, $http) {
  $http.get('js/data.json').success(function(data){
  	$scope.artists = data;
+ 	$scope.artistOrder = 'name';
  }); 
 }]);
